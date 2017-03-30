603fba17d029268783588f6d7d11410d6137c4df
ruanxingzhi <ruanxingzhi@gmail.com>
2017-03-30 15:22:40 +0800

    传题的前置技能

--- content.php
+++ content.php
@@ -2,8 +2,14 @@
 
 $content = array
 (
+    '安装' => array(
     '安装UOJ'=>'post/安装.md',
     '维护UOJ'=>'post/维护.md'
+    ),
+    
+    '题目' => array(
+        '传题的前置技能'=>'post/传题的前置技能.md'
+    )
 );
 
 ?>
